cd85abf6df2ee19f70ac7173e8f6ab2f8dcde07f
wxh <qc_b3c518cd2fe64de3872b69a0013a0a21@git.code.tencent.com>
2019-08-14 22:00:55 +0800

    Updates

---
+++ precache-manifest.6e555d7d9349381ac2e99b0b4706b0c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "366c3aa4bd767d58ee365ed80ef7468b",
+    "url": "/gitpagetest/index.html"
+  },
+  {
+    "revision": "23d00e0f40ed5ba2287d",
+    "url": "/gitpagetest/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "5bd4827ecaacd54bb735",
+    "url": "/gitpagetest/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "23d00e0f40ed5ba2287d",
+    "url": "/gitpagetest/static/js/2.d0d3b1fe.chunk.js"
+  },
+  {
+    "revision": "5bd4827ecaacd54bb735",
+    "url": "/gitpagetest/static/js/main.c6c1601d.chunk.js"
+  },
+  {
+    "revision": "8b9e054e5164e736bdd5",
+    "url": "/gitpagetest/static/js/runtime~main.e298d563.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gitpagetest/static/media/logo.5d5d9eef.svg"
+  }
+]);
